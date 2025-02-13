fc68890ae4b0587515e4f01a50b951aeda00a05f
Sequoia Kanies <sequoia.mathias.kanies@gmail.com>
2025-02-13 11:22:32 -0500

    making a useless style update to new contact page, which isnt available in the menu

--- contact/form.js
+++ contact/form.js
@@ -2,31 +2,31 @@
 import './form.css'
 import { setupForm } from './form-handler.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div class="contact-form">
-//     <h1>Contact Us</h1>
-//     <form id="contactForm">
-//       <div class="form-group">
-//         <label for="name" class="form-label">Name</label>
-//         <input type="text" id="name" name="name" class="form-input">
-//         <div class="error-name"></div>
-//       </div>
+document.querySelector('#app').innerHTML = `
+  <div class="contact-form">
+    <h1>Contact Us</h1>
+    <form id="contactForm">
+      <div class="form-group">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" id="name" name="name" class="form-input">
+        <div class="error-name"></div>
+      </div>
       
-//       <div class="form-group">
-//         <label for="email" class="form-label">Email</label>
-//         <input type="email" id="email" name="email" class="form-input">
-//         <div class="error-email"></div>
-//       </div>
+      <div class="form-group">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" id="email" name="email" class="form-input">
+        <div class="error-email"></div>
+      </div>
       
-//       <div class="form-group">
-//         <label for="message" class="form-label">Message</label>
-//         <textarea id="message" name="message" class="form-textarea"></textarea>
-//         <div class="error-message"></div>
-//       </div>
+      <div class="form-group">
+        <label for="message" class="form-label">Message</label>
+        <textarea id="message" name="message" class="form-textarea"></textarea>
+        <div class="error-message"></div>
+      </div>
       
-//       <button type="submit" class="submit-button">Send Message</button>
-//     </form>
-//   </div>
-// `
+      <button type="submit" class="submit-button">Send Message</button>
+    </form>
+  </div>
+`
 
 setupForm(document.querySelector('#contactForm'))
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const versionNumber = 3.7;
+const versionNumber = 3.8;
 
 self.addEventListener('install', e => {
     try {
